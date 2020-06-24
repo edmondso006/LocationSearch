92d35bd24121f4f0253ce8c31217fe810479826c
Jeff Edmondson <edmondso006@knights.gannon.edu>
2020-06-24 18:33:48 -0400

    Initial Commit

---
+++ LocationSearch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LocationSearch
+//
+//  Created by Jeffrey Edmondson on 6/24/20.
+//  Copyright © 2020 Jeffrey Edmondson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
